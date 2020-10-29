2ec8add6e077ae15481c4ebdbfa86827f0af6d01
leoncio <rodrigo.leoncio@sicoob.com.br>
2020-10-28 22:53:53 -0300

    primeiro commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import { React, Component }from 'react'
+import "./styles.css"
+
+class Header extends Component{
+    render(){
+       return  <header id="main-header">JSHunt</header>
+    }
+}
+
+export default Header;
